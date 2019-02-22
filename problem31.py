437a7f5131dbc12b5773be908b535a45b405b8f1
Jens Petersen <jens.petersen@dkfz.de>
2019-02-22 13:59:39 +0100

    Add solution for problem 31

--- problem31.py
+++ problem31.py
@@ -40,9 +40,25 @@ __license__ = ''
 __date__ = 'Fri Jan 22 14:02:15 2016'
 __version__ = '0.1'
 
+
 # =============================================================================
 # METHODS
 # =============================================================================
+
+def get_combinations(target, options):
+
+    options = sorted(options, reverse=True)
+
+    if len(options) == 0 or min(options) > target or target <= 0:
+        yield []
+
+    for o, option in enumerate(options):
+
+        if option > target:
+            continue
+
+        for combination in get_combinations(target - option, options[o:]):
+            yield [option] + combination
 
 
 # =============================================================================
@@ -51,10 +67,12 @@ __version__ = '0.1'
 
 def main():
 
-    options = [1, 2, 5]
-    target = 7
+    options = [1, 2, 5, 10, 20, 50, 100, 200]
+    target = 200
 
-    options = sorted(options, reverse=True)
+    all_combinations = list(get_combinations(target, options))
+    print(len(all_combinations))
+
 
 # =============================================================================
 # RUN
